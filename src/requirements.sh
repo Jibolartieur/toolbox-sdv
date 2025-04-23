8f4d4978911ba0be4561ccacc31399394935dfe6
Jibolartieur <kekeles428@gmail.com>
2025-04-23 10:02:22 +0200

    Dépendance sh

---
+++ src/requirements.sh
@@ -0,0 +1,26 @@
+sudo apt-get update && sudo apt-get install -y \
+    nmap \
+    traceroute \
+    whois \
+    curl \
+    dirb \
+    iputils-ping \
+    git \
+    nikto \
+    dnsutils \
+    sslscan \
+    whatweb \
+    python3 \
+    python3-pip \
+    golang-go \
+    libfreetype6-dev \
+    libjpeg62-turbo-dev \
+    libpng-dev \
+    libxml2-dev \
+    libzip-dev \
+    php8.2-cli \
+    php8.2-gd \
+    php8.2-curl \
+    php8.2-xml \
+    php8.2-mbstring \
+    php8.2-zip
